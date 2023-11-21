26e3056b6c867db99262f3fba08f158c79a31adc
Jesp112 <151615023+Jesp112@users.noreply.github.com>
2023-11-21 10:32:29 +0100

    Added Kommune windows
    Added Kommune windows

---
+++ Exam12/View/CreateKommuneWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ContactsEditor_MVVM.Model;
+using ContactsEditor_MVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Exam12.View
+{
+    /// <summary>
+    /// Interaction logic for CreateKommuneWindow.xaml
+    /// </summary>
+    public partial class CreateKommuneWindow : Window
+    {
+        private ContactViewModel model = new ContactViewModel(new Contact(), MainViewModel.repository);
+        public CreateKommuneWindow()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            model.CloseHandler += delegate (object sender, EventArgs e) { Close(); };
+            model.WarningHandler += delegate (object sender, MessageEventArgs e) {
+                MessageBox.Show(e.Message, "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+            };
+            DataContext = model;
+        }
+    }
+}
